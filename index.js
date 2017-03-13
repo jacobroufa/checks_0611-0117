7e953b94680da8e599b9f0a7c2363e2e776c39c0
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-03-12 23:53:59 -0500

    update parser to not sort by year, and to account for all formats of VendorID

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function getChecks (page, cols) {
                 // If the VendorID is not in the expected format, we are looking at an employee.
                 // Per FOIA response (linked below), employee ID numbers have been redacted.
                 // http://www.boarddocs.com/il/rps205/Board.nsf/files/AJENPU5FE10B/$file/PDF%201.pdf
-                if (place === 3 && !cellText.match(/^(DNE|V)\d+$/i)) {
+                if (place === 3 && !cellText.match(/^(dne|v|s|l)\d+[a-z]?$/i)) {
                     row[labels[place]] = '';
                     place++;
                 }
@@ -65,37 +65,6 @@ function getChecks (page, cols) {
     });
 
     return rows;
-}
-
-function byYears (initial, next) {
-    let date = next.CheckDate.split('/');
-    let months = {
-        1: 'January',
-        2: 'February',
-        3: 'March',
-        4: 'April',
-        5: 'May',
-        6: 'June',
-        7: 'July',
-        8: 'August',
-        9: 'September',
-        10: 'October',
-        11: 'November',
-        12: 'December'
-    };
-    let month = months[date[0]];
-    let year = date[2];
-
-    if (!initial[year]) {
-        initial[year] = {};
-    }
-    if (!initial[year][month]) {
-        initial[year][month] = [];
-    }
-
-    initial[year][month].push(next);
-
-    return initial;
 }
 
 parser.on('pdfParser_dataError', error => console.error(error.parserError));
@@ -113,11 +82,9 @@ parser.on('pdfParser_dataReady', data => {
     });
 
     console.log('filtering employees');
-    let employees = checks.filter(check => !check.VendorID)
-        .reduce(byYears, {});
+    let employees = checks.filter(check => !check.VendorID);
     console.log('filtering vendors');
-    let vendors = checks.filter(check => check.VendorID)
-        .reduce(byYears, {});
+    let vendors = checks.filter(check => check.VendorID);
 
     console.log('writing our parsed JSON');
     fs.writeFile(employeesJsonPath, JSON.stringify(employees, null, 4));
